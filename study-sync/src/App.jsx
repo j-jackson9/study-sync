3bdb3a3e4a0d2a2ff5f442aece21216c75046a51
j-jackson9 <jordancjack.20@gmail.com>
2026-05-20 23:19:28 -0500

    finished styling each component

--- study-sync/src/App.jsx
+++ study-sync/src/App.jsx
@@ -1,25 +1,56 @@
 import { useState } from 'react'
 import './App.css'
+import './styles/TaskInput.css';
+import './styles/TaskList.css';
+import './styles/ScheduleView.css';
+
+
 
 function App() {
   return (
     <>
-     <div class="header">
-        Study Sync
-     </div>
-     <div class='task-box'>
-        <div class="add-task">
-            <label>Add Task</label>
-            <form>
+     <div className="header">
+       Study✏️Sync
+      </div>
+
+      <div className="task-box">
+        <div className="add-task">
+          <h1>✅Add Task</h1>
+          <form>
             <label>Task Name</label>
-            <br></br>
-            <input type='text' class='i1' name='task-name'></input>
-          </form> 
+            <br />
+            <input type="text" className="l1" name="task-name" />
+
+            <br />
+            <label>Due Date</label>
+            <br />
+            <input type="text" className="l1" name="task-date" />
+
+            <br />
+            <label>Hours Needed</label>
+            <br />
+            <input type="text" className="l1" name="task-hours" />
+
+            <div className="btn">
+              <button className="add-button">+Add task</button>
+            </div>
+          </form>
         </div>
-        <div class="task-list">
-          <label class='l2'>Task List</label>
+
+        <div className="task-list">
+          <h1>📌Task List</h1>
+          <label><i>No tasks yet -- add one!</i></label>
         </div>
-     </div>
+      </div>
+
+      <div className="schedule">
+        <h1>📝Generated Schedule</h1>
+        <label><i>Your schedule will appear here.</i></label>
+      </div>
+
+      <div className="btn">
+        <button className="generate">Generate</button>
+      </div>
     </>
   )
 }
